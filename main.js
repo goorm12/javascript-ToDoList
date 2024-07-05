0b43faf3f53ab56ca35152102938fc481135dc2b
binwoonki <vinwoonki98@gmail.com>
2024-07-05 19:11:57 +0900

    feat : 삭제하기 버튼 추가
    - 삭제하기 버튼 클릭시 삭제하기 구현

--- main.js
+++ main.js
@@ -3,27 +3,33 @@ const todoInput = document.querySelector("#todo-form__text");
 const todoButton = document.querySelector("#todo-form__submit-button");
 const todoList = document.querySelector("#todo-form__list");
 
-function addButton(e) {
+function addTodo(e) {
   const todoText = todoInput.value.trim();
 
   e.preventDefault();
-  if (todoText === "") {
-    alert("할 일을 입력해주세요!");
-  } else {
-    const item = document.createElement("div");
+  if (todoText !== "") {
+    const div = document.createElement("div");
     const checkbox = document.createElement("input");
     const text = document.createElement("span");
     const button = document.createElement("button");
 
-    todoList.appendChild(item).classList.add("todo-form__list-content");
-    item.appendChild(checkbox).classList.add("todo-form__list--checkbox");
-    item.appendChild(text).classList.add("todo-form__list-text");
-    item.appendChild(button).classList.add("todo-form__list--remove-button");
+    todoList.appendChild(div).classList.add("todo-form__list-content");
+    div.appendChild(checkbox).classList.add("todo-form__list--checkbox");
+    div.appendChild(text).classList.add("todo-form__list-text");
+    div.appendChild(button).classList.add("todo-form__list--remove-button");
 
     text.textContent = todoText;
     button.textContent = "삭제하기";
     checkbox.type = "checkbox";
+
+    // 삭제하기 버튼
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      div.parentNode.removeChild(div);
+    });
+  } else {
+    alert("할 일을 입력해주세요!");
   }
 }
 
-todoButton.addEventListener("click", addButton);
+todoButton.addEventListener("click", addTodo);
