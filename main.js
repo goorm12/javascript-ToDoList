32ad747099fb3847ba20da11057c8d15b3e6d54f
binwoonki <vinwoonki98@gmail.com>
2024-07-05 19:47:59 +0900

    feat : 엔터키 누를 시 추가 하기 기능 추가
    - 할 일 작성 후 엔터키 누르면 추가하기 기능 추가

--- main.js
+++ main.js
@@ -1,27 +1,23 @@
 const todoDiv = document.querySelector("#todo");
 const todoInput = document.querySelector("#todo-form__text");
-const todoButton = document.querySelector("#todo-form__submit-button");
+const todoButton = document.querySelector("#todo-form__button");
 const todoList = document.querySelector("#todo-form__list");
 
 function addTodo(e) {
   const todoText = todoInput.value.trim();
 
-  e.preventDefault();
   if (todoText !== "") {
     const div = document.createElement("div");
     const checkbox = document.createElement("input");
     const text = document.createElement("span");
     const button = document.createElement("button");
-
     todoList.appendChild(div).classList.add("todo-form__list-content");
     div.appendChild(checkbox).classList.add("todo-form__list--checkbox");
     div.appendChild(text).classList.add("todo-form__list-text");
     div.appendChild(button).classList.add("todo-form__list--remove-button");
-
     text.textContent = todoText;
     button.textContent = "삭제하기";
     checkbox.type = "checkbox";
-
     // 체크박스
     checkbox.addEventListener("change", (e) => {
       if (e.target.checked) {
@@ -30,7 +26,6 @@ function addTodo(e) {
         div.style.textDecoration = "";
       }
     });
-
     // 삭제하기 버튼
     button.addEventListener("click", (e) => {
       e.preventDefault();
@@ -42,3 +37,11 @@ function addTodo(e) {
 }
 
 todoButton.addEventListener("click", addTodo);
+
+// 엔터키를 누르면 추가하기
+todoInput.addEventListener("keydown", (e) => {
+  const ENTER = 13;
+  if (e.keyCode === ENTER) {
+    addTodo();
+  }
+});
