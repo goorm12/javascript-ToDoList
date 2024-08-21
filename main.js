867470e5a90631c127158b23c9d8493de3c2ed7b
binwoonki <vinwoonki98@gmail.com>
2024-08-21 21:26:23 +0900

    refactor : 체크박스 체크 시 밑줄 긋기
    - 체크 박스 체크 시 textDecoration 적용 하기

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
 const $todoDiv = document.querySelector("#todo");
 const $todoInput = document.querySelector("#todo-form__text");
 const $todoButton = document.querySelector("#todo-form__button");
-const $todoList = document.querySelector("#todo-form__list");
+const $todoList = document.querySelector(".todo-list");
 
-const addTodo = (e) => {
-  if (e.key !== "Enter") return;
-  $todoList.innerHTML = `
-    <li>
-      <input type='checkbox' />
-      ${$todoInput.value}
-      <button class='todo-form__list--remove-button'>삭제</button>
-    </li>
-  `;
+const addTodo = () => {
+  $todoList.innerHTML =
+    `
+  <li class='todo-list__li'>
+    <input type='checkbox'/>
+    <span>${$todoInput.value}</span>
+    <button class='todo-list__remove-button'>삭제</button>
+  </li>
+  ` + $todoList.innerHTML;
+
+  $todoInput.value = "";
 };
 
-$todoInput.addEventListener("keyup", addTodo);
+const addTodoEnter = (e) => {
+  if (e.key !== "Enter") return;
+  addTodo();
+};
+
+$todoList.addEventListener("change", (e) => {
+  e.target.nextElementSibling.style.textDecoration = e.target.checked
+    ? "line-through"
+    : "none";
+});
+
+$todoButton.addEventListener("click", addTodo);
+$todoInput.addEventListener("keyup", addTodoEnter);
